bbe5ca4dc32926a4646394967c2201add8dc77d6
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2024-08-12 15:58:40 +0800

    2nd commit to Next.js

--- laranext/app/page.tsx
+++ laranext/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+import ProductCard from './components/ProductCard'
+
 export default function Index() {
   return (
     <main>
-      <h1>
-        Hello, Next.js!
-      </h1>
+      <h1>Hello, Next.js!</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard/>
     </main>
   )
 }
